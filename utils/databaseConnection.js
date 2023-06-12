9e2b0a4b8ce2c8097acbc88255be0d574f96c0cc
unknown <you@example.com>
2023-06-12 14:42:14 +0700

    add option connect to databaseConnect

--- utils/databaseConnection.js
+++ utils/databaseConnection.js
@@ -1,6 +1,13 @@
 const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE, process.env.USER_NAME, process.env.PASSWORD, {
     host: process.env.HOST,
-    dialect: process.env.DIALECT
+    dialect: process.env.DIALECT,
+    operatorAlias: false,
+    logging: false,
+    pool: {
+        max: 5,
+        idle: 30000,
+        acquire: 60000,
+    },
 });
 module.exports = sequelize;
