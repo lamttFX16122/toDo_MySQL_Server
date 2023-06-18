72f659c549800e61458c39a4c844b9ec9c175ab0
unknown <you@example.com>
2023-06-18 23:15:38 +0700

    change .env and databaseConnection.js for connecting database to Supapase

--- utils/databaseConnection.js
+++ utils/databaseConnection.js
@@ -2,6 +2,13 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE, process.env.USER_NAME, process.env.PASSWORD, {
     host: process.env.HOST,
     port: process.env.DB_PORT,
-    dialect: process.env.DIALECT
+    dialect: process.env.DIALECT,
+    dialectOptions:
+        process.env.DB_SSL === 'true' ? {
+            ssl: {
+                required: true,
+                rejectUnauthorized: false
+            }
+        } : {}
 });
 module.exports = sequelize;
